edf200ff23bc2e2d7a643f25be94212e48469f8a
ericrcwu001 <ericrw2025@student.cis.edu.hk>
2023-01-03 21:11:34 +0800

    added items

---
+++ src/main/java/net/eric/vanillaplus/item/ModItems.java
@@ -0,0 +1,9 @@
+package net.eric.vanillaplus.item;
+
+import net.eric.vanillaplus.VanillaPlus;
+
+public class ModItems {
+    public static void registerModItems() {
+        VanillaPlus.LOGGER.info("Registering Mod Items for vanillaplus");
+    }
+}
