e21168aed097ce6e94f5ffc798ff692efefb5dd9
ericrcwu001 <ericrw2025@student.cis.edu.hk>
2023-01-03 22:28:16 +0800

    added ModBlocks + Mythril Block

--- src/main/java/net/eric/vanillaplus/VanillaPlus.java
+++ src/main/java/net/eric/vanillaplus/VanillaPlus.java
@@ -1,5 +1,6 @@
 package net.eric.vanillaplus;
 
+import net.eric.vanillaplus.block.ModBlocks;
 import net.eric.vanillaplus.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -12,5 +13,6 @@ public class VanillaPlus implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
